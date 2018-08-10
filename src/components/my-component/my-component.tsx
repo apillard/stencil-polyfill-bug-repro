87167f482c113264ecb3c0e904f9f10d671a638d
Adam Pillard <agentbilly@gmail.com>
2018-08-10 10:21:25 -0500

    Demonstrate stencil failing to polyfill

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -11,6 +11,12 @@ export class MyComponent {
   @Prop() last: string;
 
   render() {
+    console.log('This line should always log');
+    const bool = Number.isFinite(45);
+    const otherBool = Number.isNaN(45);
+    console.log('Value of bool is', bool);
+    console.log('Value of otherBool is', otherBool);
+    console.log('IE never logs these lines because it fails silently');
     return (
       <div>
         Hello, World! I'm {this.first} {this.last}
